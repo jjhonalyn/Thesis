607a716d92aa4fd49babab5e7c0e34f34c6f66a4
jjhonalyn <127106297+jjhonalyn@users.noreply.github.com>
2023-09-06 13:55:17 +0800

    jb

--- psytal/vite/src/views/authorized_view/views_components/AddCourse.jsx
+++ psytal/vite/src/views/authorized_view/views_components/AddCourse.jsx
@@ -13,6 +13,7 @@ export default function AddCourse({closeModal}) {
                       id="classYear"
                       name="classYear"
                       type="text"
+                      
                       placeholder='Year'
                       //value={classYear}
                       className="block w-[48%] rounded-md border-0 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-black placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6 type=text" 
